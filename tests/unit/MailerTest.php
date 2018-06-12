d4969a801d7a8c99e1b40c007b7348cb44169f39
Martin Plattner <martin.plattner@gmail.com>
2018-06-12 20:54:19 +0200

    Added tests for previously added Mailer->sendMultiple() method.

--- tests/unit/MailerTest.php
+++ tests/unit/MailerTest.php
@@ -15,6 +15,8 @@
 namespace tests\unit;
 
 use sweelix\mailjet\Mailer;
+use sweelix\mailjet\Message;
+use Yii;
 
 /**
  * Test node basic functions
@@ -47,9 +49,79 @@ class MailerTest extends TestCase
         return $component;
     }
 
+    /**
+     * Method to enable IDE auto-completion in other methods
+     * @return null|Mailer
+     * @throws \yii\base\InvalidConfigException
+     */
+    protected function getYiiMailerComponent()
+    {
+        return Yii::$app->get('mailer');
+    }
+
     public function testGetMailjetMailer()
     {
         $mailer = $this->createTestEmailComponent();
         $this->assertInstanceOf(Mailer::className(), $mailer);
+    }
+
+    /**
+     * @return Message test message instance.
+     */
+    protected function createTestMessage()
+    {
+        return Yii::$app->get('mailer')->compose();
+    }
+
+
+    public function testMultipleSend()
+    {
+        // allow disabling real tests
+        if (MAILJET_TEST_SEND === true) {
+            $mailer = Yii::$app->get('mailer');
+            $messages = [];
+            $numberOfTestMessages = 3;
+            for ($i = 1; $i <= $numberOfTestMessages; $i++) {
+                $message = $this->createTestMessage();
+                $message->setFrom(MAILJET_FROM);
+                $message->setTo(MAILJET_TO);
+                $message->setSubject('Yii MailJet test message: testMultipleSend() - (' . $i . ' of ' . $numberOfTestMessages . ')');
+                $message->setTextBody('Yii MailJet test body: testMultipleSend() - (' . $i . ' of ' . $numberOfTestMessages . ')');
+                $messages[] = $message;
+            }
+            $successCount = $mailer->sendMultiple($messages);
+            $this->assertEquals($numberOfTestMessages, $successCount);
+        }
+    }
+
+    public function testMultipleSendReturnResponseBody()
+    {
+        // allow disabling real tests
+        if (MAILJET_TEST_SEND === true) {
+            $messages = [];
+            $numberOfTestMessages = 3;
+            for ($i = 1; $i <= $numberOfTestMessages; $i++) {
+                $message = $this->createTestMessage();
+                $message->setFrom(MAILJET_FROM);
+                $message->setTo(MAILJET_TO);
+                $message->setSubject('Yii MailJet test message: testMultipleSendReturnResponseBody() -  (' . $i . ' of ' . $numberOfTestMessages . ')');
+                $message->setTextBody('Yii MailJet test body: testMultipleSendReturnResponseBody() - (' . $i . ' of ' . $numberOfTestMessages . ')');
+                $messages[] = $message;
+            }
+
+            $mailer = $this->getYiiMailerComponent();
+            $response = $mailer->sendMultiple($messages, true);
+
+            $this->assertInstanceOf(\Mailjet\Response::class, $response);
+            $this->assertEquals(200, $response->getStatus());
+            $this->assertEquals(true, $response->success());
+
+            $responseBody = $response->getBody();
+            $this->assertArrayHasKey('Messages', $responseBody);
+            $this->assertCount($numberOfTestMessages, $responseBody['Messages']);
+            for ($i = 0; $i < $numberOfTestMessages; $i++) {
+                $this->assertEquals('success', $responseBody['Messages'][$i]['Status']);
+            }
+        }
     }
 }
